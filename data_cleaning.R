ad7285586d805f247668d06b72f9e268126e100e
James Lee <jlee317@bu.edu>
2020-04-24 12:29:10 -0700

    1. change variable selection method 2. normalized numerical values

--- data_cleaning.R
+++ data_cleaning.R
@@ -13,60 +13,45 @@ library(e1071)
 
 
 
-# Reading in the data and exploring
+# reading in the data and exploring
 df <- fread('hotel_data.csv')
+View(df)
+df$V1 <- NULL
+unique(df$agent)
+summary(df == 'NULL')
 unique(df$country)
 df <- df[!country == '']
 table(df$country)
+df[hotel == 'City Hotel']
+
+# Changing column names to make it easier to type
+names(df)[names(df) == "Km from Portugal"] <- "km"
+names(df)[names(df) == "European Union"] <- "eu"
+names(df)[names(df) == "country2"] <- "country_name"
+names(df)[names(df) == 'resort'] <- 'hotel_resort'
 colnames(df)
+
+
 # add a column for arrival_date for time series analysis
 df[, arrival_date_month := match(arrival_date_month,month.name)]
 df[, arrival_date := with(df, ymd(sprintf('%04d%02d%02d', arrival_date_year, arrival_date_month, arrival_date_day_of_month)))]
 df[, reservation_status_date := as.Date(reservation_status_date)]
 df[, arrival_date_month:= month.name[arrival_date_month]]
+summary(df[, c('arrival_date_month', 'arrival_date', 'reservation_status_date')])
 
-unique(df$hotel)
 df$city <- sapply(df$hotel, function(x) if(x == 'City Hotel'){1} else {0})
 df$resort <- sapply(df$hotel, function(x) if(x == 'Resort Hotel'){1} else {0})
-
-# meal: FB, BB, HB
+df$hotel <- NULL
+# SC and Undefined are both no meal (NM)
+df[meal == 'SC'| meal == 'Undefined', meal:= 'NM']
 unique(df$meal)
-df$no_meal <- sapply(df$meal, function(x) if(x == 'SC' | x == 'Undefined') {1} else{0})
-df$full_board <- sapply(df$meal, function(x) if(x == 'FB') {1} else {0})
-df$half_board <- sapply(df$meal, function(x) if(x == 'BB') {1} else {0})
-df$bed_breakfast <- sapply(df$meal, function(x) if(x == 'HB') {1} else {0})
 
-#distribution_channel: TA/TO, Direct, Corporate, Undefined, GDS
-unique(df$distribution_channel)
-df$distribute_TA_TO <- sapply(df$distribution_channel, function(x) if(x == 'TA/TO') {1} else {0})
-df$distribute_direct <- sapply(df$distribution_channel, function(x) if(x == 'Direct') {1} else {0})
-df$distribute_corporate <- sapply(df$distribution_channel, function(x) if(x == 'Corporate') {1} else {0})
-df$distribute_undefined <- sapply(df$distribution_channel, function(x) if(x == 'Undefined') {1} else {0})
-df$distribute_gds <- sapply(df$distribution_channel, function(x) if(x == 'GDS') {1} else {0})
+# assuming those with NA children has none
+df[is.na(children), children:= 0]
 
-# market_segment: Online TA, Offline TA/TO, Direct, Corporate, Groups
-df$segment_online <- sapply(df$market_segment, function(x) if(x == 'Online TA') {1} else {0})
-df$segment_offline <- sapply(df$market_segment, function(x) if(x == 'Offline TA/TO') {1} else {0})
-df$segment_corporate <- sapply(df$market_segment, function(x) if(x == 'Corporate') {1} else {0})
-df$segment_direct <- sapply(df$market_segment, function(x) if(x == 'Direct') {1} else {0})
-df$segment_groups <- sapply(df$market_segment, function(x) if(x == 'Groups') {1} else {0})
-df$segment_complementary <- sapply(df$market_segment, function(x) if(x == 'Complementary') {1} else {0})
-df$segment_aviation <- sapply(df$market_segment, function(x) if(x == 'Aviation') {1} else {0})
-df$segment_undefined <- sapply(df$market_segment, function(x) if(x == 'Undefined') {1} else {0})
-unique(df$market_segment)
-
-# deposit type: Non Refund, Refundable, No Deposit
-unique(df$deposit_type)
-df$deposit_non_refund <- sapply(df$deposit_type, function(x) if(x == 'Non Refund') {1} else {0})
-df$deposit_refundable <- sapply(df$deposit_type, function(x) if(x == 'Refundable') {1} else {0})
-df$deposit_no_deposit <- sapply(df$deposit_type, function(x) if(x == 'No Deposit') {1} else {0})
-
-# customer_type: Transient, Transient Party, Contract, Group
-unique(df$customer_type)
-df$customer_transient <- sapply(df$customer_type, function(x) if(x == 'Transient') {1} else {0})
-df$customer_transient_party <- sapply(df$customer_type, function(x) if(x == 'Transient-Party') {1} else {0})
-df$customer_contract <- sapply(df$customer_type, function(x) if(x == 'Contract') {1} else {0})
-df$customer_group <- sapply(df$customer_type, function(x) if(x == 'Group') {1} else {0})
+# set NAs for agent and company to be 0
+df[is.na(agent), agent:= 0]
+df[is.na(company), company:= 0]
 
 # Storing the reservations that have been canceled and that have not been canceled 
 canceled_row <- nrow(df[is_canceled == 1])
@@ -81,46 +66,85 @@ table(df$is_canceled)
 
 # Joining the weather data with the hotel data
 weather <- fread('weather.csv')
-View(weather)
+
 weather$date <- as.Date(weather$date, '%m/%d/%y')
+
 
 # Resort hotel is Algarve, city hotel is Lisbon
 df <- merge(x = df, y = weather, by.x = c('arrival_date', 'city'), by.y = c('date', 'city'), all.x = TRUE)
-View(df[country == ''])
-
-
-
-
 # Removing precipitation column since there is already a rain column
 df$precipitation <- NULL
+summary(df)
 
+#factorize the categorical variables
+df$is_canceled <- factor(df$is_canceled, levels=unique(c(0, 1)))
+df$meal <- factor(df$meal, levels=unique(c('BB', 'FB', 'HB', 'NM')))
+df$customer_type <- factor(df$customer_type, levels=unique(c('Contract', 'Group', 'Transient', 'Transient-Party')))
+df$market_segment <- factor(df$market_segment, levels=unique(c('Online TA', 'Offline TA/TO', 'Groups', 'Direct', 'Corporate', 'Complementary', 'Undefined', 'Aviation')))
+df$distribution_channel <- factor(df$distribution_channel, levels=unique(c('Corporate', 'Direct', 'GDS', 'TA/TO', 'Undefined' )))
+df$deposit_type <- factor(df$deposit_type, levels=unique(c('No Deposit', 'Non Refund', 'Refundable')))
+df$reserved_room_type <- factor(df$reserved_room_type, levels=unique(c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'L', 'P')))
+df$arrival_date_month <- factor(df$arrival_date_month, levels=unique(month.name))
+df$assigned_room_type <- factor(df$assigned_room_type, levels=unique(c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'P')))
+df$reservation_status <- factor(df$reservation_status, levels=unique(c('Check-Out', 'Canceled', 'No-Show')))
+df$arrival_date_year <- factor(df$arrival_date_year, levels=unique(c(2015, 2016, 2017)))
+df$is_repeated_guest <- factor(df$is_repeated_guest, levels=unique(c(0, 1)))
+df$city <- factor(df$city, levels=unique(c(0, 1)))
 
 # Correlation matrix to decide which features to include in formula
-df2 <- copy(df)
-rmcols <- rev(seq(1,ncol(df2))[!as.logical(sapply(df2, is.numeric))])
-for (i in rmcols) df2[[i]] <- NULL
-corr <- round(cor(df2), 1)
-View(corr)
-ggcorrplot(corr, type = "lower") + ggtitle("Correlation Matrix for Numerical Features") + theme(plot.title = element_text(hjust = 0.5))
+#df2 <- copy(df)
+#rmcols <- rev(seq(1,ncol(df2))[!as.logical(sapply(df2, is.numeric))])
+#for (i in rmcols) df2[[i]] <- NULL
+#corr <- round(cor(df), 1)
+#View(corr)
+#ggcorrplot(corr, type = "lower") + ggtitle("Correlation Matrix for Numerical Features") + theme(plot.title = element_text(hjust = 0.5))
 
 
-# Changing column names to make it easier to type
-names(df)[names(df) == "Km from Portugal"] <- "km"
-names(df)[names(df) == "European Union"] <- "eu"
+#exclude country & country name, and change date variable to number
+cdf <- copy(df)
+cdf$country <- NULL
+cdf$country_name <- NULL
+fromDate <- min(cdf$arrival_date)
+cdf$datestamp <- as.integer(cdf$arrival_date - fromDate)
+cdf$day_of_week <- weekdays(cdf$arrival_date)
+cdf$day_of_week <- factor(cdf$day_of_week, levels=unique(c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')))
+cdf$arrival_date <- NULL
+cdf$resort <- NULL
+# IMPORTANT: reservation status shouldn't be included as it will give out the answer
+cdf$reservation_status <- NULL
+cdf$reservation_status_date <- NULL
+cdf$arrival_date_week_number <- NULL
+cdf$hotel_resort <- NULL
 
-df$is_canceled[df$is_canceled == 1] <- 'Yes'
-df$is_canceled[df$is_canceled == 0] <- 'No' 
+#normalize numerical values
+#leadtime, stays_in_weekend_nights, stays in week nights, adults, children, babies
+# previous cancellations, previous bookings not canceled, booking changes, days in waiting list, adr
+# car parking spaces, total special requests, km, eu, max_temp, average_temp, min_temp, rain, date_stamp
+summary(df)
+preproc <- preProcess(cdf[,c(3, 7:11, 16, 17, 20, 24, 26:35)], method=c('range'))
+cdf[,c(3, 7:11, 16:17, 20, 24, 26:35)] <- predict(preproc, cdf[,c(3, 7:11, 16:17, 20, 24, 26:35)])
 
-df$is_canceled <- factor(df$is_canceled)
+
 # Creating a training set and test with an 85/15 split
-test.size <- nrow(df) * 0.15
-df[, test:= 0]
-df[sample(nrow(df), test.size), test:= 1]
+set.seed(5)
+test.size <- nrow(cdf) * 0.15
+cdf[, test:= 0]
+cdf[sample(nrow(cdf), test.size), test:= 1]
 
-df.test <- df[test == 1]
-df.train <- df[test == 0]
-
+df.test <- cdf[test == 1]
+df.train <- cdf[test == 0]
+df.train.sample.size <- 5000
+df.train.sample <- df.train[sample(nrow(df.train), df.train.sample.size)]
+y.train.sample <- df.train.sample$is_canceled
 y.test <- df.test$is_canceled
 y.train <- df.train$is_canceled
+
+#variable selection
+rf <- randomForest(is_canceled ~., data = df.train.sample)
+plot(rf)
+variable_selection <- importance(rf)
+importance <- data.table(cbind(variable=rownames(variable_selection), variable_selection))
+importance$MeanDecreaseGini <- as.numeric(importance$MeanDecreaseGini)
+importance[order(-MeanDecreaseGini)]
 
 
--- models.R
+++ models.R
@@ -1,94 +1,125 @@
 
 
 # Baseline model which is a decision tree that does not include any weather or country data 
-baseline_f1 <- as.formula(is_canceled ~  lead_time + adults + is_repeated_guest + previous_cancellations + previous_bookings_not_canceled 
-                          + booking_changes + total_of_special_requests + resort + city + segment_direct 
-                          + segment_corporate + segment_groups)
+baseline_f1 <- as.formula(is_canceled ~  deposit_type + 
+                            km + lead_time + market_segment + total_of_special_requests + adr + 
+                             datestamp + agent + arrival_date_month + average_temp + day_of_week + customer_type +
+                            required_car_parking_spaces + arrival_date_day_of_month + max_temp + assigned_room_type + min_temp + 
+                            previous_cancellations + stays_in_week_nights + booking_changes)
 
-baseline_model <- rpart(baseline_f1, df.train, method="class", control = rpart.control(cp = 0.001))
+baseline_model <- rpart(baseline_f1, df.train.sample, method="class", control = rpart.control(cp = 0.001))
+plot(baseline_model)
+baseline.test <- predict(baseline_model, df.test, type ='class')
+confusionMatrix(table(y.test, baseline.test))
 
-# random forest with boosting
+# boosted tree model
 fitControl <- trainControl(## 10-fold CV
   method = "repeatedcv",
   number = 10,
   ## repeated ten times
   repeats = 10)
-nested_rfboost <- train(f1, data = df.train, 
+b.tree <- train(baseline_f1, data = df.train.sample, 
                  method = "gbm", 
                  trControl = fitControl,
                  ## This last option is actually one
                  ## for gbm() that passes through
                  verbose = FALSE)
-nested_rfboost
-
+b.tree
+btree.test <- predict(b.tree, df.test)
+btree.results <- confusionMatrix(table(y.test, btree.test))
 # results:
-# n.trees, accuracy, kappa
-#  50, 0.744, 0.489
-# 100, 0.776, 0.553
-# 150, 0.779, 0.557
-# 50, 0.777, 0.553
-# 100, 0.786, 0.573
-# 150, 0.791, 0.581
-# 50, 0.781, 0.561
-# 100, 0.791, 0.582
-# 150, 0.796, 0.591
+# interaction.depth  n.trees  Accuracy   Kappa    
+# 1                   50      0.7561208  0.5101001
+# 1                  100      0.7944802  0.5879913
+# 1                  150      0.8014011  0.6020481
+# 2                   50      0.7978587  0.5949158
+# 2                  100      0.8148598  0.6293046
+# 2                  150      0.8223988  0.6445237
+# 3                   50      0.8075992  0.6146879
+# 3                  100      0.8236397  0.6470497
+# 3                  150      0.8271587  0.6541081
+# The final values used for the model were n.trees = 150, 
+# interaction.depth = 3, shrinkage = 0.1
+# and n.minobsinnode = 10.
 
 # lasso
-nested_lasso <- train(f1, data = df.train, 
+lasso <- train(baseline_f1, data = df.train.sample, 
                  method = "glmnet", 
                  family = "binomial",
                  trControl = fitControl,
                  ## This last option is actually one
                  ## for gbm() that passes through
                  verbose = FALSE)
-nested_lasso
+lasso
+lasso.test <- predict(lasso, df.test)
+lasso.results <- confusionMatrix(table(y.test, lasso.test))
 # results
-# alpha = 0.55, lambda = 0.00044, accuracy = 0.733
+# alpha  lambda        Accuracy   Kappa    
+# 0.10   0.0004446642  0.7567385  0.5127203
+# 0.10   0.0044466421  0.7549583  0.5090571
+# 0.10   0.0444664213  0.7522375  0.5032541
+# 0.55   0.0004446642  0.7571183  0.5134794
+# 0.55   0.0044466421  0.7552583  0.5096049
+#0.55   0.0444664213  0.7421790  0.4819657
+#1.00   0.0004446642  0.7578183  0.5148759
+#1.00   0.0044466421  0.7568380  0.5126856
+#1.00   0.0444664213  0.6933184  0.3814665
+# The final values used for the model were alpha = 1 and
+# lambda = 0.0004446642.
+
 
 # decision tree
-nested_dtree <- train(f1, data = df.train, 
+dtree <- train(baseline_f1, data = df.train.sample, 
                       method = "rpart", 
                       trControl = fitControl, 
                       tuneLength = 10
                       ## This last option is actually one
                       ## for gbm() that passes through
                       )
-nested_dtree
-# cp = 0.00154, accuracy = 0.7803, kappa = 0.561
+dtree
+dtree.test <- predict(dtree, df.test)
+dtree.results <- confusionMatrix(table(y.test, dtree.test))
+dtree.results
+# cp           Accuracy   Kappa    
+# 0.004467912  0.7996178  0.5994439
+# 0.005144869  0.7942974  0.5889220
+# 0.005280260  0.7932174  0.5867241
+# 0.005686434  0.7917379  0.5837771
+# 0.008529651  0.7897574  0.5798992
+# 0.013809911  0.7871566  0.5747565
+# 0.022339561  0.7556752  0.5107524
+# 0.032629299  0.7383976  0.4754647
+# 0.056661251  0.6896382  0.3744908
+# 0.329000812  0.5774482  0.1433437
+# The final value used for the model was cp = 0.004467912.
+
 # random forest
-nested_rf <- train(f1, data = df.train, 
+rf <- train(baseline_f1, data = df.train, 
                    method = "rf", 
                    trControl = fitControl,
                    ## This last option is actually one
                    ## for gbm() that passes through
                    verbose = FALSE)
-nested_rf
+rf.test <- predict(rf, df.test)
+rf.results <- confusionMatrix(table(y.test, dtree.test))
+rf.results
 
 
 # Naive bayes
-nested_bayes <- train(f1, data = df.train, 
+bayes <- train(baseline_f1, data = df.train.sample, 
                       method = "nb", 
                       trControl = fitControl, 
                       tuneLength = 10
                       ## This last option is actually one
                       ## for gbm() that passes through
 )
-nested_bayes
+bayes
 
 
-# Formula that includes all the predictors with corr coeff >= 0.1 as well as all the weather and country attributes
-f1 <- as.formula(is_canceled ~  lead_time + adults + is_repeated_guest + previous_cancellations + previous_bookings_not_canceled + booking_changes + days_in_waiting_list + required_car_parking_spaces
-                 + total_of_special_requests + km + resort + city + distribute_TA_TO + distribute_direct + distribute_corporate + segment_direct + segment_corporate + segment_groups 
-                 + deposit_no_deposit + deposit_non_refund + customer_transient_party)
-
-# Decision tree based on formula
-fit.tree <- rpart(f1, df.train, method="class", control = rpart.control(cp = 0.001))
-
-yhat.tree <- predict(fit.tree, df.train)
-mse_train <- mean((y.train - yhat.tree)^2)
-yhat.tree.test <- predict(fit.tree, df.test)
-mse.test <- mean((y.test - yhat.tree.test)^ 2)
-summary(fit.tree)
+baseline_model <- rpart(baseline_f1, df.train, method="class", control = rpart.control(cp = 0.001))
+plot(baseline_model)
+baseline.test <- predict(baseline_model, df.test, type ='class')
+confusionMatrix(table(y.test, baseline.test))
 
 # Random forest based on formula
 fit.rndfor <- randomForest(f1, df.train, ntree=500, do.trace=0, importance=TRUE)
@@ -103,11 +134,11 @@ conf <- fit.rndfor$confusion
 
 conf
 
-df.train[is_canceled=="Yes"] = 1
-df.train[is_canceled=="No"] = 0
+
 
 # boosted tree
-fit.btree <- gbm(f1, data = df.train, n.trees = 5000, distribution='bernoulli', interaction.depth = 2, shrinkage = 0.001, cv.folds=5)
+summary(df.train)
+fit.btree <- gbm(baseline_f1, data = df.train, n.trees = 5000, distribution='bernoulli', interaction.depth = 2, shrinkage = 0.001, cv.folds=5)
 gbm.perf(fit.btree)
 
 summary(fit.btree)
@@ -120,16 +151,15 @@ best.iter = gbm.perf(fit.btree, method="cv")
 
 fitControl = trainControl(method="cv", number=5, returnResamp = "all")
 
-model2 <- train(f1, data=df.train, method="gbm",distribution="bernoulli", trControl=fitControl, verbose=F, tuneGrid=data.frame(.n.trees=best.iter, .shrinkage=0.01, .interaction.depth=1, .n.minobsinnode=1))
-
-model2
+cv.btree <- train(baseline_f1, data=df.train, method="gbm",distribution="bernoulli", trControl=fitControl, verbose=F, tuneGrid=data.frame(.n.trees=best.iter, .shrinkage=0.01, .interaction.depth=1, .n.minobsinnode=1))
+cv.btree
 
 mPred = predict(model2, df.train, na.action = na.pass)
 
 
 
 
-postResample(mPred, mydata$diabetes)
+
 
 
 
