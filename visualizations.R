5f582b1879777d71e8222bfeaa5c26d038ea35aa
Mani <realmanisingh@gmail.com>
2020-04-25 21:18:11 -0400

    Added all graphs

--- visualizations.R
+++ visualizations.R
@@ -1,7 +1,8 @@
 
 # Cancellations per month based on whether or not the hotel is a city hotel or resort hotel
-df_time <- df[,100*(sum(is_canceled)/.N), by= .(arrival_date_month, hotel)]
-ggplot(df_time, aes(x = factor(arrival_date_month, levels= c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",  "December")), y = V1, fill= hotel)) + geom_histogram(stat = 'identity', position = 'dodge') + labs(title = "Cancellations per Month", y = "Cancellation %" , x = 'Month') + scale_fill_discrete(name="Hotel") + theme(plot.title = element_text(hjust = 0.5)) + scale_fill_manual("Hotel", values = c("City Hotel" = "deepskyblue", "Resort Hotel" = "orange"))
+df_time <- df_country[,100*(sum(cancel_int)/.N), by= .(arrival_date_month, city)]
+labels <-  c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+ggplot(df_time, aes(x = factor(arrival_date_month, levels= c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",  "December")), y=V1, fill=arrival_date_month)) + geom_histogram(stat = 'identity', position = 'dodge') + labs(title = "Cancellations per Month", y = "Cancellation %" , x = 'Month') + scale_fill_discrete(name="Hotel") + theme(plot.title = element_text(hjust = 0.5)) + scale_x_discrete(labels=labels) #+ scale_fill_manual("Hotel", values = c("City Hotel" = "deepskyblue", "Resort Hotel" = "orange"))
 
 
 
@@ -9,12 +10,13 @@ ggplot(df_time, aes(x = factor(arrival_date_month, levels= c("January", "Februar
 df_country <- df
 df_country[, cancel_int := 0] 
 df_country[is_canceled==1, cancel_int:=1]
-df_temp <- df_country[, sum(cancel_int), by=.(country)]
+df_temp <- df_country[, 100*(sum(cancel_int)/.N), by=.(country)]
 
 df_temp_sub <- df_temp[country=="PRT" | country=="GBR" | country=="FRA" | country=="ESP" | country=="ITA" | country=="DEU"]
 
 ggplot(df_temp_sub, aes(x=country, y=V1, fill=country)) + geom_bar(stat = "identity") + 
-  labs(title = "Total Cancellations for Top 6 Countries", y="Total Cancellations", x="Countries")
+  labs(title = "Cancellation Rates for Top 6 Countries", y="Cancellation Percentage", x="Countries") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 
 # Correlation Matrix for analyzing relevant features
